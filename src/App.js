51ef725cba129dbe6dd2c8c5647f790d172a7f33
佐藤瑠星 <ryusei@satounoMacBook-Pro-2.local>
2024-11-13 15:41:40 +0900

    new_configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const FetchData = () => {
+const callLambdaFunction = async () => {
+  try {
+    const response = await fetch('https://1ws95y0j1h.execute-api.ap-northeast-1.amazonaws.com/dev', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  fetch("https://1ws95y0j1h.execute-api.ap-northeast-1.amazonaws.com/dev")
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch(() => alert("error"));
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
-  return <></>;
+    const result = await response.json();
+    console.log(result); // データを表示
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    alert(`Error: ${error.message}`);
+  }
 };
-
-export default FetchData;

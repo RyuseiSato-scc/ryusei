3d55acf4d8a817a0c39dba3844df9e623605dcc1
佐藤瑠星 <ryusei@satounoMacBook-Pro-2.local>
2024-11-13 16:26:26 +0900

    new_configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
-function App(){
-const FetchData = () => {
+import React, { useEffect } from "react";
 
-  fetch("https://1ws95y0j1h.execute-api.ap-northeast-1.amazonaws.com/dev")
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch(() => alert("error"));
+function App() {
+  // FetchData関数を定義
+  const FetchData = () => {
+    fetch("https://1ws95y0j1h.execute-api.ap-northeast-1.amazonaws.com/dev")
+      .then((res) => res.json()) // JSON形式に変換
+      .then((json) => console.log(json)) // 取得したデータをコンソールに出力
+      .catch(() => alert("error")); // エラー発生時にアラート
+  };
 
-  return <></>;
-};
+  // コンポーネントがマウントされた時にFetchDataを呼び出す
+  useEffect(() => {
+    FetchData();
+  }, []); // 空の依存配列で一度だけ実行される
 
+  return (
+    <div>
+      <h1>APIデータの取得</h1>
+    </div>
+  );
 }
 
 export default App;
